6ddde4e2f0d6671613f07e7a3f2ee95b1782a96d
jacques <janneslambrecht@hotmail.com>
2024-10-01 11:46:13 +0200

    update path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Button from "@/components/common/Button";
 
 export default function Home() {
   return (
     <div>
       <main>
-      
+        Hello
+        <Button>Click me</Button>
       </main>
       <footer>
       </footer>
